1804d0447f421c94a6dedc357fea9e1570954138
usaeed <uzmansaeed@gmail.com>
2018-01-16 23:37:44 +0100

    my first project

---
+++ src/firstproject/ExampleIf.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firstproject;
+
+
+public class ExampleIf {
+      public static void main (String[] args){
+          
+          int x = 100;
+          int y = 1000;
+          
+          if (x < y){
+              System.out.println("x is less than y");
+              
+          }else if (x == y){
+              System.out.println("x is equal to y");
+              
+          }else if (x > y){
+              System.out.println("x is greater than y");
+              
+          }
+      }
+    
+}
---
+++ src/firstproject/MyFirstClass.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firstproject;
+
+/**
+ *
+ * @author James
+ */
+
+public class MyFirstClass {
+    public static void main (String[] args){
+    
+        System.out.println("HelloWorld");
+    int x = 10;
+    int y = 20;
+    int result = x + y;
+    int result2 = x % y;
+    System.out.println("Result = " + result);
+    System.out.println("Result2 = " + result);
+    
+    }
+    
+}
